f5d1b88763c99022dce8ea5182718a980421dcb4
Obigba Ebruski <o.ebruski@gmail.com>
2020-08-19 01:06:38 +0100

    Add project files.

---
+++ Feels/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Feels.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
